643ae0093cf2525155b27a4851ad87b983e48291
jwh <jwh@tigensoft.co.kr>
2025-11-07 13:48:43 +0900

    springdb2/JPA

--- springdb2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ springdb2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "itemName", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+
+    // JPA는 public또는 protected의 기본 생성자가 필수
     public Item() {
     }
 
