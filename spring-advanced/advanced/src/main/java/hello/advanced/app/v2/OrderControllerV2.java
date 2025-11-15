173aefa5f329f25c3d65a0f6a0da162bbc3001ee
jwh <jwh5096@naver.com>
2025-11-15 21:47:32 +0900

    spring-advanced/log trace

--- spring-advanced/advanced/src/main/java/hello/advanced/app/v2/OrderControllerV2.java
+++ spring-advanced/advanced/src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -1,10 +1,8 @@
 package hello.advanced.app.v2;
 
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
 import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
