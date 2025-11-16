7e72d096fd14ef4833088349cf5c434705fba1c4
jwh <jwh5096@naver.com>
2025-11-16 18:10:19 +0900

    spring-advanced/strategy pattern

--- spring-advanced/advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV1.java
+++ spring-advanced/advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV1.java
@@ -2,6 +2,9 @@ package hello.advanced.trace.strategy.code.strategy;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 필드에 전략을 보관하는 방식
+ */
 @Slf4j
 public class ContextV1 {
     private Strategy strategy;
