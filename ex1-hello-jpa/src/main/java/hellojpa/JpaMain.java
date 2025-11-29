b90fcf6d112d1a0e95625a984da196708eefdb95
jwh <jwh5096@naver.com>
2025-11-29 23:05:33 +0900

    ex-hello-jpa/고급 매핑 (상속관계 매핑, @MappedSuperclass)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,28 +16,19 @@ public class  JpaMain {
         tx.begin();
         //code
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            Movie movie = new Movie();
+            movie.setDirector("봉준호");
+            movie.setActor("배우");
+            movie.setName("이름");
+            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("memeber1");
-
-            // team.addMember(member) 사용시 제거, 둘중 하나에만 설정하는것이 좋음
-//            member.changeTeam(team);
-            em.persist(member);
-
-            team.addMember(member);
+            em.persist(movie);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("m = " + m);
-            }
+            Member findMovie = em.find(Member.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,5 +39,30 @@ public class  JpaMain {
 
         em.close();
         emf.close();
+    }
+
+    private static void codeV1(EntityManager em) {
+        Team team = new Team();
+        team.setName("TeamA");
+        em.persist(team);
+
+        Member member = new Member();
+        member.setUsername("memeber1");
+
+        // team.addMember(member) 사용시 제거, 둘중 하나에만 설정하는것이 좋음
+//            member.changeTeam(team);
+        em.persist(member);
+
+        team.addMember(member);
+
+        em.flush();
+        em.clear();
+
+        Member findMember = em.find(Member.class, member.getId());
+        List<Member> members = findMember.getTeam().getMembers();
+
+        for (Member m : members) {
+            System.out.println("m = " + m);
+        }
     }
 }
