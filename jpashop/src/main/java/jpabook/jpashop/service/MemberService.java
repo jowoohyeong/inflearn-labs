3684ea3aea1c17cca5f746218fd9ac6e93e63cc8
jwh <jwh5096@naver.com>
2025-12-03 13:58:37 +0900

    jpashop2/API development

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -26,6 +26,7 @@ public class MemberService {
         return member.getId();
     }
 
+
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
@@ -42,5 +43,14 @@ public class MemberService {
 
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    /**
+     * 회원 수정
+     */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
