94ca8a8eff3687fd5a8539d0b0a68111eaad7d18
jwh <jwh5096@naver.com>
2025-12-05 15:22:49 +0900

    data-jpa project

---
+++ data-jpa/src/main/groovy/study/data_jpa/DataJpaApplication.groovy
@@ -0,0 +1,13 @@
+package study.data_jpa
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DataJpaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DataJpaApplication, args)
+	}
+
+}
