c1e2f93edaa024027c2786dd4392f7c29573118e
jwh <jwh5096@naver.com>
2025-12-06 19:33:31 +0900

    data-jpa/공통 인터페이스 기능

--- data-jpa/src/test/groovy/study/data_jpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/groovy/study/data_jpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
-import study.data_jpa.repository.MemberRepository;
 
 import java.util.List;
 
