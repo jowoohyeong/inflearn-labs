c7416c7f41e46df7d5462c08958e4ae5b66bea76
jwh <jwh5096@naver.com>
2025-12-07 18:02:12 +0900

    data-jpa/Auditing

--- data-jpa/src/main/groovy/study/data_jpa/DataJpaApplication.groovy
+++ data-jpa/src/main/groovy/study/data_jpa/DataJpaApplication.groovy
@@ -2,7 +2,11 @@ package study.data_jpa
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class DataJpaApplication {
 
@@ -10,4 +14,8 @@ class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication, args)
 	}
 
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
--- data-jpa/src/test/groovy/study/data_jpa/entity/MemberTest.java
+++ data-jpa/src/test/groovy/study/data_jpa/entity/MemberTest.java
@@ -3,9 +3,11 @@ package study.data_jpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -15,6 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberTest {
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
@@ -45,7 +50,26 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.getTeam() = " + member.getTeam());
         }
+    }
 
+    @Test
+    @Transactional
+    void jpaEventBaseEntity() throws InterruptedException {
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(1000);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        System.out.println("findMember.getCreatedDate() = " + findMember.getCreatedDate());
+        System.out.println("findMember.getLastModifiedDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreateBy() = " + findMember.getCreateBy());
+        System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
     }
 
 
