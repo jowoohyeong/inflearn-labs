10f50efcafdb26759ccd810980cc85c9084d1283
jwh <jwh5096@naver.com>
2025-12-08 12:38:09 +0900

    data-jpa/Web 확장 - 페이징과 정렬

--- data-jpa/src/main/groovy/study/data_jpa/controller/MemberController.java
+++ data-jpa/src/main/groovy/study/data_jpa/controller/MemberController.java
@@ -2,9 +2,12 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -23,8 +26,25 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/membersV1")
+    public Page<Member> listV1(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+    @GetMapping("/membersV2")
+    public Page<MemberDto> listV2(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
+    @RequestMapping(value = "/members_page", method = RequestMethod.GET)
+    public String list2(@PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+    return null;
+    }
+
     @PostConstruct
     void init() {
-        memberRepository.save(new Member("member1", 20));
+        for (int i = 0; i < 100 ; i++){
+            memberRepository.save(new Member("member"+ i, i));
+        }
+
     }
 }
