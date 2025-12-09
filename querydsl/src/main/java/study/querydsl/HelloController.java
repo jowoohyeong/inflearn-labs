de12727269ad220ff953250ffcf455096f3956af
jwh <jwh5096@naver.com>
2025-12-09 13:50:06 +0900

    querydsl/예제 도메인 모델 생성

---
+++ querydsl/src/main/java/study/querydsl/HelloController.java
@@ -0,0 +1,15 @@
+package study.querydsl;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello!";
+    }
+}
